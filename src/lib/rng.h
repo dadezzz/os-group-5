83ca1d253a65b9959fc8e583e42c8ab17956f73d
Davide Zarantonello <davide@zarantonello.dev>
2026-05-04 12:24:17 +0200

    feat: complete makefile and add rng implementation (#3)

---
+++ src/lib/rng.h
@@ -0,0 +1,20 @@
+#ifndef RNG_H
+#define RNG_H
+
+#include <stdint.h>
+
+// Holds the state of the RNG.
+//
+// This struct doesn't support concurrent access and thus must not be shared
+// between threads. If you want to pass an RNG to a new thread use
+// `rng_new_thread_state`.
+typedef struct {
+  uint64_t data[4];
+} RNGState;
+
+uint64_t rng_next(RNGState* state);
+RNGState* rng_new_main_state(uint64_t seed);
+RNGState* rng_new_thread_state(RNGState* mainState);
+void rng_drop_state(RNGState* state);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,1 +1,11 @@
-int main () {return 0;}
+#include <stdio.h>
+
+#include "lib/rng.h"
+
+int main(void) {
+  RNGState* rng_state = rng_new_main_state(42);
+  printf("%lu\n", rng_next(rng_state));
+
+  rng_drop_state(rng_state);
+  return 0;
+}
