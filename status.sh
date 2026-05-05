a9423385a4d9a28d53498306e0fc7f12c2fa5936
Davide Zarantonello <davide@zarantonello.dev>
2026-05-05 15:29:56 +0200

    feat: status.sh (#11)

--- status.sh
+++ status.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-# TODO:
-#
-# - send SIGUSR1 to the restaurant process (pid stored in /tmp/restaurant.pid);
+PID_PATH=/tmp/restaurant.pid
+
+if [ ! -f "$PID_PATH" ] ; then
+  echo "$PID_PATH doesn't exist. Restaurant executable needs to be running."
+  exit 1
+fi
+
+PID=$(cat $PID_PATH)
+
+if [ ! $(ps -A -o uid,pid | grep -E "^\s$(id -u)\s$PID" ) ] ; then
+  echo "The file $PID_PATH exists but doesn't point to a process owned by the current user."
+  exit 1
+fi
+
+pkill -USR1 "$PID"
