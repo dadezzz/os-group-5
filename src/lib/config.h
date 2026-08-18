9fc2d557ee00e464aa655ad02bf57b03e282aa0f
Davide Zarantonello <davide@zarantonello.dev>
2026-08-18 15:37:20 +0200

    fix: memory management in config

--- src/lib/config.h
+++ src/lib/config.h
@@ -1,19 +1,17 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include <stdint.h>
-
 #include "result.h"
 
 typedef struct {
-  uint32_t num_cooks;
-  uint32_t num_waiters;
-  uint32_t max_customers;
-  uint32_t total_customers;
+  int num_cooks;
+  int num_waiters;
+  int max_customers;
+  int total_customers;
   const char* menu_file;
   const char* resources_file;
   double game_speed;
-  uint32_t random_seed;
+  int random_seed;
 } Config;
 
 Config* config_new();
--- src/main.c
+++ src/main.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
-
 #include "lib/config.h"
 #include "lib/result.h"
 #include "lib/rng.h"
 
-int main(void) {
+int main() {
   Result result;
 
   Config* config = config_new();
   result = config_load(config);
   if (result != RESULT_OK) {
+    config_drop(config);
     return result;
   }
 
@@ -18,7 +17,6 @@ int main(void) {
   // load resources file
 
   RNGState* rng_state = rng_new_main_state(config->random_seed);
-  printf("%lu\n", rng_next(rng_state));
 
   // Cleanup.
   rng_drop_state(rng_state);
