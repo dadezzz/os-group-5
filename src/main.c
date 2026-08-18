22e81e3e6828b8f894580ee0d92ec14214f05a50
Davide Zarantonello <davide@zarantonello.dev>
2026-08-18 12:30:49 +0200

    feat: parse config (#23)

--- src/main.c
+++ src/main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 
+#include "lib/config.h"
+#include "lib/result.h"
 #include "lib/rng.h"
 
 int main(void) {
-  RNGState* rng_state = rng_new_main_state(42);
+  Result result;
+
+  Config* config = config_new();
+  result = config_load(config);
+  if (result != RESULT_OK) {
+    return result;
+  }
+
+  // load menu file
+
+  // load resources file
+
+  RNGState* rng_state = rng_new_main_state(config->random_seed);
   printf("%lu\n", rng_next(rng_state));
 
+  // Cleanup.
   rng_drop_state(rng_state);
+  config_drop(config);
+
   return 0;
 }
