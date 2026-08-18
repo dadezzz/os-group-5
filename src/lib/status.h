0c9ee981d9553b0eb86cae4dc514cbfc855162c7
Davide Zarantonello <davide@zarantonello.dev>
2026-08-18 11:04:15 +0200

    feat: status.h (#18)

---
+++ src/lib/status.h
@@ -0,0 +1,9 @@
+#ifndef STATUS_H
+#define STATUS_H
+
+// We can add more error types here
+typedef enum Errors {
+  STATUS_OK, STATUS_INVALID_ORDER, STATUS_RESOURCE_UNAVAILABLE, STATUS_INVALID_PARAMETER, STATUS_FILE_NOT_FOUND
+};
+
+#endif
