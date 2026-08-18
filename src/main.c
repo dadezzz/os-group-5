e5d1abe493b9cbd56ef1babfc3a0a64dc79e242a
Davide Zarantonello <davide@zarantonello.dev>
2026-08-18 15:41:36 +0200

    feat: resources parsing (#27)

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
+#include <stdint.h>
+
 #include "lib/config.h"
+#include "lib/resources.h"
 #include "lib/result.h"
 #include "lib/rng.h"
 
@@ -12,14 +15,23 @@ int main() {
     return result;
   }
 
+  Resources* resources = resources_new();
+  result = resources_load(config->resources_file, resources);
+  if (result != RESULT_OK) {
+    resources_drop(resources);
+    config_drop(config);
+    return result;
+  }
+
   // load menu file
 
   // load resources file
 
-  RNGState* rng_state = rng_new_main_state(config->random_seed);
+  RNGState* rng_state = rng_new_main_state((uint64_t)config->random_seed);
 
   // Cleanup.
   rng_drop_state(rng_state);
+  resources_drop(resources);
   config_drop(config);
 
   return 0;
