af29c9b08d1f8c0d1a0440111d45c150c65b3fab
tisatoelia <devele14@gmail.com>
2026-08-20 17:28:17 +0200

    feat: tid attributes

--- src/lib/state/dish-ticket.h
+++ src/lib/state/dish-ticket.h
@@ -1,13 +1,16 @@
 #ifndef DISH_TICKET_H
 #define DISH_TICKET_H
 
+#include <pthread.h>
 #include "../dishes/dishes.h"
 
 typedef struct {
   Dish* dish;
+  pthread_t customer_tid;
+  pthread_t waiter_tid;
 } DishTicket;
 
-void dish_ticket_init(DishTicket* dish_ticket, Dish* dish);
+void dish_ticket_init(DishTicket* dish_ticket, Dish* dish, pthread_t customer_tid, pthread_t waiter_tid);
 
 void dish_ticket_drop(void* arg);
 
