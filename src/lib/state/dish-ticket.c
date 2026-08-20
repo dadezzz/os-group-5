90587390ad4752bdc9287c666278c3a32b68cda1
Elia <devele14@gmail.com>
2026-08-20 17:19:44 +0200

    feat: cook thread 🧑‍🍳 (#41)

--- src/lib/state/dish-ticket.c
+++ src/lib/state/dish-ticket.c
@@ -1,15 +1,16 @@
 #include "dish-ticket.h"
 
-#include "../fifo-queue.h"
+#include <stdlib.h>
 
 void dish_ticket_init(DishTicket* dish_ticket, Dish* dish) {
   dish_ticket->dish = dish;
 }
 
-void dish_ticket_drop(DishTicket* dish_ticket) {
-  if (dish_ticket == nullptr) {
+void dish_ticket_drop(void* arg) {
+  if (arg == nullptr) {
     return;
   }
 
+  DishTicket* dish_ticket = arg;
   free(dish_ticket->dish);
 }
---
+++ src/lib/threads/cook.c
@@ -0,0 +1,68 @@
+#include "cook.h"
+
+#include <pthread.h>
+#include <string.h>
+
+#include "../fifo-queue.h"
+#include "../result.h"
+#include "../state/dish-ticket.h"
+
+Result cook_assign(DishTicket* dish_ticket, Cook* cook) {
+  // TODO: use mutex to lock the queue while writing.
+
+  queue_push(&cook->task_q, dish_ticket);
+  cook->queued_time += dish_ticket->dish->cook_time;
+
+  return RESULT_OK;
+}
+
+static Result cook_run(Cook* cook) {
+  // TODO
+
+  return RESULT_OK;
+}
+
+static void* cook_thread(void* arg) {
+  Result* result = malloc(sizeof(Result));
+  if (result == nullptr) {
+    return nullptr;
+  }
+
+  Result run_result = cook_run(arg);
+  memcpy(result, &run_result, sizeof(Result));
+
+  return result;
+}
+
+Result cook_init(Cook* cook) {
+  queue_init(&cook->task_q, sizeof(DishTicket));
+  cook->queued_time = 0;
+
+  int result = pthread_create(&cook->tid, nullptr, cook_thread, cook);
+  if (result != 0) {
+    return RESULT_THREAD_CREATION_FAILED;
+  }
+
+  return RESULT_OK;
+}
+
+Result cook_drop(Cook* cook) {
+  if (cook == nullptr) {
+    return RESULT_OK;
+  }
+
+  Result* run_result;
+  int join_result = pthread_join(cook->tid, &run_result);
+
+  queue_drop(&cook->task_q, dish_ticket_drop);
+
+  if (join_result != 0) {
+    return RESULT_THREAD_JOIN_FAILED;
+  }
+
+  if (run_result == nullptr) {
+    return RESULT_OUT_OF_MEMORY;
+  }
+
+  return *run_result;
+}
