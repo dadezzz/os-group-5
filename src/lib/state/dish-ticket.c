26d8a6266e5575c7856194825957aa115e6343f6
Elia <devele14@gmail.com>
2026-08-20 17:31:27 +0200

    feat: tid attributes 🆕 (#42)

--- src/lib/state/dish-ticket.c
+++ src/lib/state/dish-ticket.c
@@ -1,9 +1,15 @@
 #include "dish-ticket.h"
 
+#include <pthread.h>
 #include <stdlib.h>
 
-void dish_ticket_init(DishTicket* dish_ticket, Dish* dish) {
+void dish_ticket_init(DishTicket* dish_ticket,
+                      Dish* dish,
+                      pthread_t customer_tid,
+                      pthread_t waiter_tid) {
   dish_ticket->dish = dish;
+  dish_ticket->customer_tid = customer_tid;
+  dish_ticket->waiter_tid = waiter_tid;
 }
 
 void dish_ticket_drop(void* arg) {
