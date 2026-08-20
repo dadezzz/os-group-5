6d547c2dccc0ffd1ab0a4a4790675de8fd805375
tisatoelia <devele14@gmail.com>
2026-08-20 17:29:57 +0200

    fix: format

--- src/lib/state/dish-ticket.c
+++ src/lib/state/dish-ticket.c
@@ -3,7 +3,10 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-void dish_ticket_init(DishTicket* dish_ticket, Dish* dish, pthread_t customer_tid, pthread_t waiter_tid) {
+void dish_ticket_init(DishTicket* dish_ticket,
+                      Dish* dish,
+                      pthread_t customer_tid,
+                      pthread_t waiter_tid) {
   dish_ticket->dish = dish;
   dish_ticket->customer_tid = customer_tid;
   dish_ticket->waiter_tid = waiter_tid;
