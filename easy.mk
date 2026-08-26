bef481336279cf88adb955311c9d074d16b766e7
Davide Zarantonello <davide@zarantonello.dev>
2026-08-26 16:07:03 +0200

    feat: add make submission target (#84)

---
+++ easy.mk
@@ -0,0 +1,38 @@
+SOURCES  = $(shell find code/ -name '*.c')
+OBJECTS  = $(patsubst code/%.c,build/objects/%.o,$(SOURCES))
+DEPS     = $(patsubst %.o,%.d,$(OBJECTS))
+
+all: build
+
+# Generate .d files to recompile when header files change.
+CPPFLAGS += -MMD -MP
+# Set C standard to 23 and enable some stricter diagnostics.
+CFLAGS   += -std=gnu23 -pthread
+# Add libm for ceil and pow functions.
+LDFLAGS  += -pthread -lm
+
+# Include the header files generated with -MMD in $CPPFLAGS.
+-include $(DEPS)
+
+build: build/restaurant
+
+# Link the restaurant executable.
+build/restaurant: $(OBJECTS)
+	@echo "LD $@"
+	@$(CC) $(LDFLAGS) $(OBJECTS) -o $@
+
+# Create object and compile_commands files for each source file.
+build/objects/%.o: code/%.c
+	@echo "CC $@"
+	@mkdir -p $(dir $@)
+	@$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
+
+# Clean the build/ dir and tmp pid file.
+clean:
+	@rm -r build/ /tmp/restaurant.pid
+
+# Run ./bootstrap.sh, optionally with args passed via ARGS
+run:
+	@./bootstrap.sh $(ARGS)
+
+.PHONY: clean run
--- src/status.sh
+++ src/status.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
+source ./src/lib/result.sh
+
 PID_PATH=/tmp/restaurant.pid
 
 if [ ! -f "$PID_PATH" ] ; then
   echo "$PID_PATH doesn't exist. Restaurant executable needs to be running."
-  exit 1
+  exit $RESULT_PID_FILE_NOT_FOUND;
 fi
 
 PID=$(cat $PID_PATH)
 
 if [ ! -d "/proc/$PID" ] || [ "$(stat -c '%u' "/proc/$PID" 2>/dev/null)" != "$(id -u)" ]; then
   echo "The file $PID_PATH exists but doesn't point to a process owned by the current user."
-  exit 1
+  exit $RESULT_PID_FILE_NOT_OPENED;
 fi
 
 kill -USR1 "$PID"
